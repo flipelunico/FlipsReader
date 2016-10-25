878416dc66c9f67675308f02d3e9a527b05e77e6
git <flipelunico@gmail.com>
2016-10-25 00:55:07 -0300

    html!

--- app/src/main/java/com/flipsoft/flipreader/app/Fragment1.java
+++ app/src/main/java/com/flipsoft/flipreader/app/Fragment1.java
@@ -3,11 +3,17 @@ package com.flipsoft.flipreader.app;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.flipsoft.flipreader.app.Parser.MyTagHandler;
+import com.flipsoft.flipreader.app.Parser.RssTagHandler;
+import com.flipsoft.flipreader.app.Parser.URLImageParser;
 
 /**
  * Created by Flipelunico on 18-10-16.
@@ -25,6 +31,14 @@ public class Fragment1 extends Fragment {
         View view = inflater.inflate(R.layout.fragment1, container, false);
         TextView feed_title = (TextView) view.findViewById(R.id.feed_title);
         TextView feed_content = (TextView) view.findViewById(R.id.feed_content);
+
+        String mime = "text/html";
+        String encoding = "utf-8";
+
+        //WebView myWebView = (WebView) view.findViewById(R.id.myWebView);
+        //myWebView.getSettings().setJavaScriptEnabled(true);
+
+
         TextView feed_author =  (TextView) view.findViewById(R.id.feed_author);
 
         final Bundle args = getArguments();
@@ -36,8 +50,14 @@ public class Fragment1 extends Fragment {
             String origin_title = args.getString("origin_title");
             //mImage.setImageResource(getImageDrawable(getActivity(), position));
             feed_title.setText(title);
+            //myWebView.loadDataWithBaseURL(null, content, mime, encoding, null);
+
             String textHTML = content.replaceAll("\\\\n","<br>");
-            feed_content.setText(Html.fromHtml(textHTML));
+            String textHTML2 = textHTML.replaceAll("\\\\","");
+            //feed_content.setText(Html.fromHtml(textHTML));
+            Log.i("Flipelunico","HTML : " + textHTML2);
+            feed_content.setText(Html.fromHtml(textHTML2, new URLImageParser(feed_content, getContext()), new MyTagHandler()));
+            //feed_content.setText();
             feed_author.setText(origin_title + " by " + author);
         }
 
