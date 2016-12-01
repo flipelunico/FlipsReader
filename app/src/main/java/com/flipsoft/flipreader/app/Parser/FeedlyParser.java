3df1f69d72fda46d66a77644d7897f99ea15697e
flipelunico <flipelunico@gmail.com>
2016-12-01 12:43:32 -0300

    new token

--- app/src/main/java/com/flipsoft/flipreader/app/Parser/FeedlyParser.java
+++ app/src/main/java/com/flipsoft/flipreader/app/Parser/FeedlyParser.java
@@ -38,7 +38,7 @@ public class FeedlyParser {
         private String CATEGORIES_URL = "http://cloud.feedly.com/v3/categories";
         private String SUBSCRIPTIONS_URL = "http://cloud.feedly.com/v3/subscriptions";
 		private String ENTRIES_URL = "http://cloud.feedly.com/v3/streams/contents?streamId=user/45572cdc-c7de-425f-bc9a-11e08b224fab/category/Android";
-        private String Token = "OAuth AzbvRrDhWdB0seGrkhh3g2dz5W941-2XMNPRO7vVhlR9mDrcHSgdiK7Z8zuoNY71IndDUEejb221HhNd4qooDfx4It1dooI4_8vqcjaOZE5JIsYoFtu-jhnBVW7ii3b-KYK2wmG8aCNwsiMeQXpEBTcxLN67f9DIRmubfGKvaRZ4rGty1XmFszpBr8oFc7g1287IOOmS1peMxk5iaPi4V7JpDosQ:feedlydev";
+        private String Token = "OAuth A1dTZrLPu-9ewUD2aA-y8aFN63JkzMmyS5F50EE1Yw7uFD1QZMwOrTabzDV8Td88lKVhpJcJ4fKkC5URvEXOUIkRIfbNu58ATXi5645sUhv2mJ7JZEJTnQB7CWNe1Z-LViL7LDoiy3veVh_J-kdy_wWv_7gSCD1WWMHw6miFt4e0GEC3re6s7s6OxqwtBHq2wh7VBJOiUhwDzG0aV4xfvX9LvNUs:feedlydev";
         private Context mContext;
 
         /*
